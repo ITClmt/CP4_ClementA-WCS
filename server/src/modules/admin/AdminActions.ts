ec881028444a76e084b6169cae9f3e17c1be10ab
Clément.A <125081819+ITClmt@users.noreply.github.com>
2025-04-02 18:19:07 +0200

    fix: fixed api routes

---
+++ server/src/modules/admin/AdminActions.ts
@@ -0,0 +1,53 @@
+import type { RequestHandler } from "express";
+import User from "../../../database/models/user.model";
+import jwt from "jsonwebtoken";
+
+const loginAdmin: RequestHandler = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+      res.status(401).json({ error: "Identifiants invalides" });
+    } else if (!user.isAdmin) {
+      res.status(403).json({ error: "Accès refusé" });
+    } else {
+      const payload = {
+        userId: user._id,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        isAdmin: user.isAdmin,
+      };
+
+      if (!process.env.JWT_SECRET) {
+        throw new Error(
+          "Vous n'avez pas configuré votre JWT SECRET dans le .env",
+        );
+      }
+
+      const token = jwt.sign(payload, process.env.JWT_SECRET as string, {
+        expiresIn: "1y",
+      });
+      res
+        .cookie("CP4auth", token, {
+          httpOnly: true,
+          sameSite: "strict",
+          secure: process.env.NODE_ENV === "production",
+          maxAge: 31536000000,
+        })
+        .json({
+          message: "Connexion réussie",
+          id: user._id,
+          email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          isAdmin: user.isAdmin,
+        });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default { loginAdmin };
--- server/src/router.ts
+++ server/src/router.ts
@@ -7,23 +7,54 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Define auth routes
-import authUserActions from "./modules/auth/authUserActions";
+import authUserActions from "./modules/admin/AdminActions";
 
 router.post("/api/auth/login", authUserActions.loginAdmin);
-router.post("/api/auth/logout", authUserActions.logoutAdmin);
 
 // Appointment routes
 
 import appointmentActions from "./modules/appointment/appointmentActions";
 import authMiddleware from "./middleware/authMiddleware";
 
-router.post("/api/appointments", authMiddleware.verify, appointmentActions.createAppointment);
-router.get("/api/appointments", authMiddleware.verify, authMiddleware.checkAdmin, appointmentActions.readAppointments);
-router.get("/api/appointments/:email", authMiddleware.verify, appointmentActions.readAppointmentsByEmail);
-router.get("/api/appointments/:id", authMiddleware.verify, authMiddleware.checkAdmin, appointmentActions.readAppointmentById);
-router.put("/api/appointments/:id", authMiddleware.verify, authMiddleware.checkAdmin, appointmentActions.updateAppointment);
-router.delete("/api/appointments/:id", authMiddleware.verify, authMiddleware.checkAdmin, appointmentActions.deleteAppointment);
-router.delete("/api/users/appointments/:id", authMiddleware.verify, appointmentActions.deleteUserAppointments);
+router.post(
+  "/api/appointments",
+  authMiddleware.verify,
+  appointmentActions.createAppointment,
+);
+router.get(
+  "/api/appointments",
+  authMiddleware.verify,
+  authMiddleware.checkAdmin,
+  appointmentActions.readAppointments,
+);
+router.get(
+  "/api/appointments/:email",
+  authMiddleware.verify,
+  appointmentActions.readAppointmentsByEmail,
+);
+router.get(
+  "/api/appointments/:id",
+  authMiddleware.verify,
+  authMiddleware.checkAdmin,
+  appointmentActions.readAppointmentById,
+);
+router.put(
+  "/api/appointments/:id",
+  authMiddleware.verify,
+  authMiddleware.checkAdmin,
+  appointmentActions.updateAppointment,
+);
+router.delete(
+  "/api/appointments/:id",
+  authMiddleware.verify,
+  authMiddleware.checkAdmin,
+  appointmentActions.deleteAppointment,
+);
+router.delete(
+  "/api/users/appointments/:id",
+  authMiddleware.verify,
+  appointmentActions.deleteUserAppointments,
+);
 
 // User routes
 
@@ -31,7 +62,7 @@ import userActions from "./modules/user/userActions";
 
 router.post("/api/users", userActions.createUser);
 router.post("/api/users/login", userActions.loginUser);
-
+router.get("/api/users/logout", userActions.logoutUser);
 
 /* ************************************************************************* */
 
